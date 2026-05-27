cd1aaf8bbbd43f6584e31776cb1e539d6547d68a
Samuel Valentim <52916865+samuel-valentimm@users.noreply.github.com>
2026-05-27 02:35:52 -0300

    feat: Ajustes nos comentarios

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ from database import ClienteService
 
 app = Flask(__name__)
 
-# 1. ROTA DA TELA INICIAL (Agora abre o painel de boas-vindas)
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# 2. ROTA DE CLIENTES (Abre a tela bonitona de gerenciamento)
 @app.route("/clientes")
 def clientes():
     dados_clientes = ClienteService.listar_clientes()
     total_clientes = len(dados_clientes)
     return render_template("clientes.html", dados_clientes=dados_clientes, total_clientes=total_clientes)
 
-# 3. ROTA PARA ADICIONAR CLIENTE
 @app.route("/adicionar", methods=["POST"])
 def adicionar():
     nome = request.form.get("nome")
@@ -25,7 +22,6 @@ def adicionar():
     # Após adicionar, volta para a tela de clientes
     return redirect(url_for("clientes"))
 
-# 4. ROTA PARA DELETAR CLIENTE
 @app.route("/deletar/<int:id_cliente>")
 def deletar(id_cliente):
     ClienteService.deletar_cliente(id_cliente)
