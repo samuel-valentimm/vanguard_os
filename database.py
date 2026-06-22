46b62e1f30b5857fc31ded85a4989dee2ff0e90c
Valentim <svs987059091@gmail.com>
2026-06-22 13:50:06 -0300

    feat: Att da database.py

--- database.py
+++ database.py
@@ -2,8 +2,15 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
-load_dotenv()
+load_dotenv(override=True)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+print(f"DEBUG: URL={SUPABASE_URL}")
+print(f"DEBUG: KEY={SUPABASE_KEY}")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise Exception("Variáveis de ambiente não carregadas! Verifique o arquivo .env")
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
